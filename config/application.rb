08bcb636b750137a57e735a8f4abadaa12c1ad53
Sunny Garg <sunny.shiva@gmail.com>
2016-01-03 23:01:36 +0530

    precompile for heroku

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module Shoppy
       # Load application's model / class decorators
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
+        config.assets.initialize_on_precompile = false
       end
 
       # Load application's view overrides
@@ -35,6 +36,5 @@ module Shoppy
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.assets.initialize_on_precompile = false
   end
 end
